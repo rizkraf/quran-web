732cc4b1d38b9e8e156677ee22bf04fd9f55220f
Rizky Rafi Azhara <azhararizkyrafi@gmail.com>
2022-06-18 10:16:21 +0700

    create skeleton card component

---
+++ src/components/SurahListsSkeleton.vue
@@ -0,0 +1,14 @@
+<template>
+  <div
+    class="box-border w-full bg-white p-4 flex gap-3 items-center shadow-md animate-pulse rounded-lg shadow-gray-200/50"
+  >
+    <div class="h-9 w-9 bg-slate-200 rounded-full"></div>
+    <div>
+      <div class="bg-slate-200 h-4 w-32 mb-4 rounded-sm"></div>
+      <div class="bg-slate-200 h-3.5 w-40 rounded-sm"></div>
+    </div>
+    <div class="ml-auto">
+      <div class="bg-slate-200 h-6 w-16 rounded-sm"></div>
+    </div>
+  </div>
+</template>
